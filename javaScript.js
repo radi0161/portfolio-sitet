6518c4a7e088f718ef6a2cbfad1ab4177fa82dd0
radi0161 <radi0161@stud.kea.dk>
2019-06-19 01:07:58 +0200

    final

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,9 @@
+window.addEventListener("load", init);
+
+function init() {
+    document.querySelector(".toggle-btn").addEventListener("click", toggleSidebar)
+}
+
 function toggleSidebar() {
     document.getElementById("sidebar").classList.toggle('active');
 }
